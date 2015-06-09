8e633f885fc0a407daa77afb279d0fb3f47a17f1
LuBollani <luc_bollani@hotmail.com>
2015-06-09 15:39:10 -0300

    O CodigoJogo estava no ignore, entao na dava pra adicionar novas classes, agora da !

--- CodigoJogo/src/implementations/combate/CEngine.java
+++ CodigoJogo/src/implementations/combate/CEngine.java
@@ -21,21 +21,23 @@ import implementations.personagens.herois.HRexus;
 import implementations.personagens.herois.HSilvana;
 
 public class CEngine {
+	
+	public static ArrayList<AbsPersonagem> listaH;
 
 	public static void CombatEngine (ArrayList<AbsPersonagem> Herois, ArrayList<AbsPersonagem> Viloes) { // recebe um ArrayList com herois e outro com viloes
-		ArrayList<AbsPersonagem> listaH = new ArrayList<AbsPersonagem> ();// lista que vai conter os herois ordenados
-			AbsPersonagem Durden = HDurden.getInstancia();
-			AbsPersonagem MDR = HMDR.getInstancia();
-			AbsPersonagem Oleg = HOleg.getInstancia();
-			AbsPersonagem Ozob = HOzob.getInstancia();
-			AbsPersonagem Rexus = HRexus.getInstancia();
-			AbsPersonagem Silvana = HSilvana.getInstancia();
-			listaH.add(Durden);
-			listaH.add(MDR);
-			listaH.add(Oleg);
-			listaH.add(Ozob);
-			listaH.add(Rexus);
-			listaH.add(Silvana);
+		listaH = new ArrayList<AbsPersonagem> ();// lista que vai conter os herois ordenados
+		AbsPersonagem Durden = HDurden.getInstancia();
+		AbsPersonagem MDR = HMDR.getInstancia();
+		AbsPersonagem Oleg = HOleg.getInstancia();
+		AbsPersonagem Ozob = HOzob.getInstancia();
+		AbsPersonagem Rexus = HRexus.getInstancia();
+		AbsPersonagem Silvana = HSilvana.getInstancia();
+		listaH.add(Durden);
+		listaH.add(MDR);
+		listaH.add(Oleg);
+		listaH.add(Ozob);
+		listaH.add(Rexus);
+		listaH.add(Silvana);
 		ArrayList<AbsPersonagem> listaV = new ArrayList<AbsPersonagem> (); // lista que vai conter os viloes ordenados
 		ArrayList<AbsPersonagem> listaI = new ArrayList<AbsPersonagem> (); // lista que vai conter a ordem de jogada
 		Random random = new Random(); // gerador de numeros randomicos
