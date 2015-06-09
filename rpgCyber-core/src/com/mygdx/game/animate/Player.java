5063aab27f84aafb7219e0864dde8ddf90302d95
leocvf <leocvfilho@gmail.com>
2015-06-09 18:04:32 -0300

    colision 2.0

--- rpgCyber-core/src/com/mygdx/game/animate/Player.java
+++ rpgCyber-core/src/com/mygdx/game/animate/Player.java
@@ -5,11 +5,19 @@ public class Player {
 	public static Animator ani1 = new Animator("palhaco.png");
 	public static Animator ani2 = new Animator("link.png");
 	public static Animator ani = ani1;
+	public static int v = 5;
 	public static void change(){
 		if(ani == ani1){
 			ani = ani2;
 		}else{
 			ani = ani1;
+		}
+	}
+	public static void speed(){
+		if(v == 5){
+			v = v*2;
+		}else{
+			v = v/2;
 		}
 	}
 }
