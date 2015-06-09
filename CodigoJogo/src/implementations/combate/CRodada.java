8f6275cd53d18db3b3def2fb6d2dd8d3e027722b
ovm357 <ovm357@gmail.com>
2015-06-09 18:30:50 -0300

    Rodada eataque basico yay

--- CodigoJogo/src/implementations/combate/CRodada.java
+++ CodigoJogo/src/implementations/combate/CRodada.java
@@ -12,7 +12,6 @@ package implementations.combate;
 import java.util.ArrayList;
 
 import implementations.personagens.AbsPersonagem;
-import implementations.personagens.skills.Skill;
 import implementations.inventario.*;
 
 import java.util.Scanner;
@@ -91,24 +90,74 @@ public class CRodada {
 		temp = Jogadores.get(contP);
 		Jogadores.remove(contP);
 		Jogadores.add(mov, temp);
-		
-		//Reatribui as variaveis de posi��o aos objetos afetados pela troca
-		for (int cont = 0; cont < Jogadores.size(); cont++){
-			Jogadores.get(cont).pos = cont;
-		}
 	}
 	
-	public static String getAlvos (Skill S, AbsPersonagem ator) {
+	public static String getAlvos (int skill, AbsPersonagem ator) {
 		
-		 if (S.tipoAlvo == 1) {
-			 if (ator.tipo == 1) return "melee";
-			 else return "ranged";
-		 }
-		 
-		 else if (S.tipoAlvo == 2) return "amigo";
-		 else if (S.tipoAlvo == 3) return "semAlvo";
+		switch (skill) {
+			
+			case 0:
+				if (ator.skill0.tipoAlvo == 1) {
+					if (ator.tipo == 1) return "melee";
+					else return "ranged";
+				}
+				else if (ator.skill0.tipoAlvo == 2) return "amigo";
+				else if (ator.skill0.tipoAlvo == 3) return "semAlvo";
+			
+			case 1:
+				if (ator.skill1.tipoAlvo == 1) {
+					if (ator.tipo == 1) return "melee";
+					else return "ranged";
+				}
+				else if (ator.skill1.tipoAlvo == 2) return "amigo";
+				else if (ator.skill1.tipoAlvo == 3) return "semAlvo";
+			
+			case 2:
+				if (ator.skill2.tipoAlvo == 1) {
+					if (ator.tipo == 1) return "melee";
+					else return "ranged";
+				}
+				else if (ator.skill2.tipoAlvo == 2) return "amigo";
+				else if (ator.skill2.tipoAlvo == 3) return "semAlvo";
+				
+			case 3:
+				if (ator.skill3.tipoAlvo == 1) {
+					if (ator.tipo == 1) return "melee";
+					else return "ranged";
+				}
+				else if (ator.skill3.tipoAlvo == 2) return "amigo";
+				else if (ator.skill3.tipoAlvo == 3) return "semAlvo";
 		
-		 else return null;
+		}
+		
+		return null;
+		
+	}
+	
+	public static void atacar (int trgt, int acao, AbsPersonagem ator) {
+		double weaponDam;
+		
+		if (ator.tipo == 1) weaponDam = ator.danoArma*(1 + (ator.forca*ator.buffForcaValor)/50)+(0.96+(ator.level/25))*0.5; //com melhor arma 100 dano, 100 for�a/percep, lvl 50: 250/3 (min) - 250 (medio) - 500 (max) - 1000 (crit)
+		else weaponDam = ator.danoArma*(1 + (ator.percepcao*ator.buffPercepcaoValor)/50)+(0.96+(ator.level/25))*0.5; //com pior arma 4 dano, 15 for�a/percep, lvl 1: 1 (min) - 4 (medio) - 8 max - 16 (crit)
+		
+		switch (acao) {
+			
+			case 0:
+				ator.skill0.execute(CEngine.listaV, CEngine.listaH, weaponDam, trgt, ator);
+				break;	
+			
+			case 1:
+				ator.skill1.execute(CEngine.listaV, CEngine.listaH, weaponDam, trgt, ator);
+				break;	
+			
+			case 2:
+				ator.skill2.execute(CEngine.listaV, CEngine.listaH, weaponDam, trgt, ator);
+				break;	
+				
+			case 3:
+				ator.skill3.execute(CEngine.listaV, CEngine.listaH, weaponDam, trgt, ator);
+				break;		
+		}
 	}
 	
 	@Deprecated
@@ -356,8 +405,8 @@ public class CRodada {
 	}
 	
 	public static void AI (ArrayList <AbsPersonagem> Herois, ArrayList <AbsPersonagem> Viloes,  ArrayList <AbsPersonagem> Lista, int contP, int contI) {
-		int atk, trgt = 1, dano, resistencia, fator;
-		double weaponDam, armadura;
+		int atk, trgt = 1;
+		double weaponDam;
 		boolean flag = true;
 		Random random = new Random(); // gerador de numeros randomicos
 		AbsPersonagem temp = new PersonGenerico();
@@ -372,10 +421,6 @@ public class CRodada {
 				Viloes.remove(contP);
 				Viloes.add(contP+1, temp);
 				
-				for (int cont = 0; cont < Viloes.size(); cont++){
-					Viloes.get(cont).pos = cont;
-				}
-				
 				flag = false;
 			}				
 		}
@@ -386,10 +431,6 @@ public class CRodada {
 			Viloes.remove(contP);
 			Viloes.add(contP-1, temp);
 			
-			for (int cont = 0; cont < Viloes.size(); cont++){
-				Viloes.get(cont).pos = cont;
-			}
-			
 			flag = false;
 		}
 		
@@ -399,10 +440,10 @@ public class CRodada {
 			if (Viloes.get(contP).tipo == 1) weaponDam = Viloes.get(contP).danoArma*(1 + (Viloes.get(contP).forca*Viloes.get(contP).buffForcaValor)/50)+(0.96+(Viloes.get(contP).level/25))*0.5;
 			else weaponDam = Viloes.get(contP).danoArma *(1 + (Viloes.get(contP).percepcao*Viloes.get(contP).buffPercepcaoValor)/50)+(0.96+(Viloes.get(contP).level/25))*0.5;
 			
-			if (atk <= 50) Viloes.get(contP).Skill0(Herois, Viloes, weaponDam, trgt, Viloes.get(contP));
-			else if ((atk > 50) && (atk <= 75)) Viloes.get(contP).Skill1(Herois, Viloes, weaponDam, trgt, Viloes.get(contP));
-			else if ((atk > 75) && (atk <= 90)) Viloes.get(contP).Skill2(Herois, Viloes, weaponDam, trgt, Viloes.get(contP));
-			else if ((atk > 90) && (atk <= 100)) Viloes.get(contP).Skill3(Herois, Viloes, weaponDam, trgt, Viloes.get(contP));
+			if (atk <= 50) Viloes.get(contP).skill0.execute(Herois, Viloes, weaponDam, trgt, Viloes.get(contP));
+			else if ((atk > 50) && (atk <= 75)) Viloes.get(contP).skill1.execute(Herois, Viloes, weaponDam, trgt, Viloes.get(contP));
+			else if ((atk > 75) && (atk <= 90)) Viloes.get(contP).skill2.execute(Herois, Viloes, weaponDam, trgt, Viloes.get(contP));
+			else if ((atk > 90) && (atk <= 100)) Viloes.get(contP).skill3.execute(Herois, Viloes, weaponDam, trgt, Viloes.get(contP));
 				
 		}
 		
