939c624e0bafd72a287c34ea4b2f5c793a33c265
JTStark <joao.baraldi61@gmail.com>
2015-06-09 22:03:21 -0300

    Adicionando a historia ao inicio do jogo

--- rpgCyber-core/src/com/mygdx/game/New.java
+++ rpgCyber-core/src/com/mygdx/game/New.java
@@ -23,7 +23,21 @@ public class New extends GameStart {
 		ScreenCreator.setGameInstance(this);
 		
 		try {
-			ScreenCreator.addAndGo(new TextLevel("0la"), new TextHUB());
+			ScreenCreator.addAndGo(new TextLevel("O ano eh 2119, os personagens encontram-se em Delta City, "
+											   + "uma gigantesca metropole riquissima erguida sobre a decadencia "
+											   + "da antiga Nova York. É de noite, o tempo está quente, o céu "
+											   + "está com nuvens de poeira e foligem provenientes dos carros que "
+											   + "encobrem a luz das estrelas e tornam parcialmente turva a luz da "
+											   + "lua que invade o quarto do dr. Norman Walker, um empreendedor, cuja empresa "
+											   + "esta comecando a ameacar as maiores.\n"
+											   + "O telefone toca, e trás uma noticia nada agradavel: Ashley, a filha do "
+											   + "dr. Walker foi sequestrada pela CEO de uma dessas grandes empresas.\n"
+											   + "Desesperado, Norman resolve pedir ajuda ao dr. Angus Silvana, outro empresario "
+											   + "de respeito que jah havia feito uma oferta pelo trabalho de sua vida.\n"
+											   + "Entao, por troca de 51% das acoes das Walker Enterprises, Silvana resolve "
+											   + "ajuda-lo, e chama seu grupo de mercenarios para resgatar a garota.\n\n"
+											   + "Walker suspeita que sua filha encontra-se no predio a oeste daquele "
+											   + "em que se encontram.\n"), new TextHUB());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
