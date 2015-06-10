4bafd99b04f2838d2475ee25cc964501c780f31c
leocvf <leocvfilho@gmail.com>
2015-06-09 23:39:51 -0300

    comb

--- rpgCyber-core/src/com/mygdx/game/colision/CComb.java
+++ rpgCyber-core/src/com/mygdx/game/colision/CComb.java
@@ -7,37 +7,30 @@ public class CComb {
 	public CComb(){
 		
 	}
-	public static void changeBau(OrthographicCamera camera,TiledMapTileLayer bau,TiledMapTileLayer bau2,TiledMapTileLayer colision){
+	public static void changeCombat(OrthographicCamera camera,TiledMapTileLayer bau,TiledMapTileLayer bau2){
 		if(bau != null){
-			if(CCClide.up(bau, camera)){
-				if(CCClide.upP(bau, camera, "chest")){
-				bau.setCell(Math.round(camera.position.x), Math.round(camera.position.y+1), bau2.getCell(0, 0));
-				
-				if(CCClide.upP(colision, camera, "blocked"))
-					colision.setCell(Math.round(camera.position.x), Math.round(camera.position.y+1), bau2.getCell(0, 0));
+			if(bau.getCell(Math.round(camera.position.x+1), Math.round(camera.position.y)) != null){
+				if(bau.getCell(Math.round(camera.position.x+1), Math.round(camera.position.y)).getTile().getProperties().get("chest")!=null){
+				bau.setCell(Math.round(camera.position.x+1), Math.round(camera.position.y), bau2.getCell(0, 0));
 				}
 			}
 			if(bau.getCell(Math.round(camera.position.x+1), Math.round(camera.position.y)) !=null){
 				if(bau.getCell(Math.round(camera.position.x+1), Math.round(camera.position.y)).getTile().getProperties().get("chest")!=null){
 				bau.setCell(Math.round(camera.position.x+1), Math.round(camera.position.y), bau2.getCell(0, 0));
-				if(colision.getCell(Math.round(camera.position.x+1), Math.round(camera.position.y)).getTile().getProperties().get("blocked")!=null)
-					colision.setCell(Math.round(camera.position.x+1), Math.round(camera.position.y), bau2.getCell(0, 0));
 				}
 			}
 			if(bau.getCell(Math.round(camera.position.x-1), Math.round(camera.position.y)) !=null){
 				if(bau.getCell(Math.round(camera.position.x-1), Math.round(camera.position.y)).getTile().getProperties().get("chest")!=null){
 				bau.setCell(Math.round(camera.position.x-1), Math.round(camera.position.y), bau2.getCell(0, 0));
-				if(colision.getCell(Math.round(camera.position.x-1), Math.round(camera.position.y)).getTile().getProperties().get("blocked")!=null)
-					colision.setCell(Math.round(camera.position.x-1), Math.round(camera.position.y), bau2.getCell(0, 0));
+			
 				}
 			}
 			if(bau.getCell(Math.round(camera.position.x-1), Math.round(camera.position.y)) !=null){
 				if(bau.getCell(Math.round(camera.position.x-1), Math.round(camera.position.y)).getTile().getProperties().get("chest")!=null){
 				bau.setCell(Math.round(camera.position.x-1), Math.round(camera.position.y), bau2.getCell(0, 0));
-				if(colision.getCell(Math.round(camera.position.x-1), Math.round(camera.position.y)).getTile().getProperties().get("blocked")!=null)
-					colision.setCell(Math.round(camera.position.x-1), Math.round(camera.position.y), bau2.getCell(0, 0));
 				}
 			}
 		}
 	}
 }
+	
