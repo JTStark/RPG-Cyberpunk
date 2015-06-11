8d316be027ac7eccf23b6e5c61c76655481d06fb
Kairo Vinícius <kairovr@gmail.com>
2015-06-10 21:50:00 -0300

    atualizando interfaceinventario

--- CodigoJogo/src/implementations/inventario/InterfaceInventario.java
+++ CodigoJogo/src/implementations/inventario/InterfaceInventario.java
@@ -1,8 +1,12 @@
 package implementations.inventario;
 
+import java.util.ArrayList;
 
 public interface InterfaceInventario {
 	public void adicionar_item(String nome_item);
 	public void remover_item(String nome_item);
 	public boolean verificar_item (String nome_item);
+	public ArrayList<String> getMochila();
+	public ArrayList<Item> getMochila(int tamanho_inicial, int tamanho_final);
+	public void setMochila(ArrayList<String> bag);
 }
